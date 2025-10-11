54428e82257b3e7045d16b656866d82360172be2
Vansh Srivastav <vanshsrivastav021@gmail.com>
2025-10-11 22:05:00 +0530

    Create views &  models

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Student Management Project</title>
+
+    <style>
+    /* The side navigation menu */
+    .sidebar {
+        margin: 0;
+        padding: 0;
+        width: 200px;
+        background-color: #f1f1f1;
+        position: fixed;
+        height: 100%;
+        overflow: auto;
+    }
+
+    /* Sidebar links */
+    .sidebar a {
+        display: block;
+        color: black;
+        padding: 16px;
+        text-decoration: none;
+    }
+
+    /* Active/current link */
+    .sidebar a.active {
+        background-color: #04AA6D;
+        color: white;
+    }
+
+    /* Links on mouse-over */
+    .sidebar a:hover:not(.active) {
+        background-color: #555;
+        color: white;
+    }
+
+    /* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+    div.content {
+        margin-left: 200px;
+        padding: 1px 16px;
+        height: 1000px;
+    }
+
+    /* On screens that are less than 700px wide, make the sidebar into a topbar */
+    @media screen and (max-width: 700px) {
+        .sidebar {
+            width: 100%;
+            height: auto;
+            position: relative;
+        }
+
+        .sidebar a {
+            float: left;
+        }
+
+        div.content {
+            margin-left: 0;
+        }
+    }
+
+    /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+    @media screen and (max-width: 400px) {
+        .sidebar a {
+            text-align: center;
+            float: none;
+        }
+    }
+    </style>
 </head>
 
 <body>
@@ -25,6 +90,24 @@
                         </button>
                     </div>
                 </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <!-- The sidebar -->
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#student">Student</a>
+                    <a href="#teacher">Teacher</a>
+                    <a href="#course">Courses</a>
+                    <a href="#enrollment">Enrollment</a>
+                    <a href="#peyment">Peyment</a>
+                </div>
+            </div>
+            <!-- Page content -->
+            <div class="col-md-9">
+                @yield('content')
             </div>
         </div>
     </div>
