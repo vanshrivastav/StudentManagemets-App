cd3ba49bda1c04c62d569c5f433cbd7c8ea24c0d
Vansh Srivastav <vanshsrivastav021@gmail.com>
2025-10-15 10:30:23 +0530

    Create Courses controller, model, views & pages

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('layout');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('students', StudentController::class);
 Route::resource('teachers', TeacherController::class);
+Route::resource('courses', CourseController::class);
